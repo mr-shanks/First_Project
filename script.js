0b0c3dc002c17f7450d21c8395d30913e3406466
Mr Shanks <mrshanks@Mrs-MacBook-Air.local>
2026-04-17 01:03:08 +0100

    I created a new folder and modified styles.css

--- script.js
+++
@@ -1,8 +0,0 @@
-inputBtnTwo.addEventListener('click', storeDataTwice);
-
-function clearAlreadyStoredData(){
-    const deleteIcon = document.getElementsByClassName('newLiClassName');
-    deleteIcon.remove();
-}
-
-clearStoredData.addEventListener('click', clearAlreadyStoredData)
